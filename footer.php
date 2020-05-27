6c5d965afd28dd5e2ebcd9c5236e90fa288481fc
kaoutargs <kaoutargs@gmail.com>
2020-05-27 13:51:27 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,132 @@
+<footer class="page-footer font-small pt-4 d-flex align-items-center text-center" id="footer">
+
+
+  <div class="container text-center">
+
+
+    <div class="row text-md-left mt-3 pb-3">
+
+
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3 ">
+        <h6 class="mb-3 font-weight-bold">Title Here</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at dignissim nunc, id maximus ex. Etiam nec dignissim elit, at dignissim enim. </p>
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a class="mx-1">
+                <div class="btn-floating btn-tw btn-network d-flex align-items-center justify-content-center">
+                  <i class="fab fa-instagram"></i>
+                </div>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="mx-1">
+                <div class="btn-floating btn-tw btn-network d-flex align-items-center justify-content-center">
+                  <i class="fab fa-facebook-f"></i>
+                </div>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="mx-1">
+                <div class="btn-floating btn-tw btn-network d-flex align-items-center justify-content-center">
+                  <i class="fab fa-twitter"></i>
+                </div>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="mx-1">
+                <div class="btn-floating btn-tw btn-network d-flex align-items-center justify-content-center">
+                  <i class="fab fa-whatsapp"></i>
+                </div>
+              </a>
+            </li>
+          </ul>
+      </div>
+
+
+      <hr class="w-100 clearfix d-md-none">
+
+
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="mb-3 font-weight-bold">About</h6>
+        <p>
+          <a href="#!">History</a>
+        </p>
+        <p>
+          <a href="#!">Our Team</a>
+        </p>
+        <p>
+          <a href="#!">Brand Guidelines</a>
+        </p>
+        <p>
+          <a href="#!">Terms & Condition</a>
+        </p>
+        <p>
+          <a href="#!">Privacy Policy</a>
+        </p>
+      </div>
+
+
+      <hr class="w-100 clearfix d-md-none">
+
+
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="mb-3 font-weight-bold">Services</h6>
+        <p>
+          <a href="#!">How to Order</a>
+        </p>
+        <p>
+          <a href="#!">Our Product</a>
+        </p>
+        <p>
+          <a href="#!">Order Status</a>
+        </p>
+        <p>
+          <a href="#!">Promo</a>
+        </p>
+        <p>
+          <a href="#!">Payment Method</a>
+        </p>
+      </div>
+
+
+      <hr class="w-100 clearfix d-md-none">
+
+
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="mb-3 font-weight-bold">Other</h6>
+
+        <p>
+          <a href="#!">Contact Us</a>
+        </p>
+
+        <p>
+          <a href="#!">Help</a>
+        </p>
+
+        <p>
+          <a href="#!">Privacy</a>
+        </p>
+
+      </div>
+
+
+    </div>
+
+  </div>
+
+</footer>
+
+
+
+
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/js/mdb.min.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang-"en" dir-"ltr">
+
+<head>
+  <meta charset="utf-8">
+<link rel="icon" href="img/1/favicon-32" type="image/png">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=no">
+
+  <title>Italian Food</title>
+
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/css/mdb.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Open+Sans:600italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="main.css">
+
+</head>
+<body>
