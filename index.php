ad490b357fe9fbcf045cb08ebdb558d5b37baa3d
kaoutargs <65283017+kaoutargs@users.noreply.github.com>
2020-05-28 09:27:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,62 +5,62 @@
 
 <header class="container-fluid">
 
-    <div class="row">
-      <div class="banner img-fluid">
-        <div class="top-left">Italian Food
-        </div>
+<div class="row">
+  <div class="banner img-fluid">
+    <div class="top-left">Italian Food
+    </div>
 
 
-                <nav class="navbar navbar-expand-lg navbar-dark mt-3 justify-content-between">
-            <a class="navbar-brand" href="#">
-            </a>
+      <nav class="navbar navbar-expand-lg navbar-dark mt-3 justify-content-between">
+  <a class="navbar-brand" href="#">
+  </a>
 
-            <div class="justify-content-end align-items-end">
-                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                      </button>
+  <div class="justify-content-end align-items-end">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
 
-                      <div class="collapse navbar-collapse" id="navbarNav">
-                                    <ul class="navbar-nav">
-                                      <li class="nav-item ml-4">
-                                        <a class="nav-link" href="#">Home</a>
-                                      </li>
-                                      <li class="nav-item ml-4">
-                                        <a class="nav-link" href="#menu">Menu</a>
-                                      </li>
-                                      <li class="nav-item ml-4">
-                                        <a class="nav-link" href="#ourstory">Our Story</a>
-                                      </li>
-                                      <li class="nav-item ml-4">
-                                        <a class="nav-link" href="#section6">Contact us</a>
-                                      </li>
-                                    </ul>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item ml-4">
+                    <a class="nav-link" href="#">Home</a>
+                  </li>
+                  <li class="nav-item ml-4">
+                    <a class="nav-link" href="#menu">Menu</a>
+                  </li>
+                  <li class="nav-item ml-4">
+                    <a class="nav-link" href="#ourstory">Our Story</a>
+                  </li>
+                  <li class="nav-item ml-4">
+                    <a class="nav-link" href="#section6">Contact us</a>
+                  </li>
+                </ul>
 
-                                    <div class="main">
+                <div class="main">
 
     <!-- Search box -->
 
-                                          <div class="form-group has-search">
-                                            <span class="fa fa-search form-control-feedback"></span>
-                                            <input type="text" class="form-control">
-                                          </div>
-                                    </div>
-                                  </div>
-                                </nav>
-                                <img id="logo"src="img/1/logo.png" class="rounded mx-auto d-block" alt="logo">
+          <div class="form-group has-search">
+            <span class="fa fa-search form-control-feedback"></span>
+            <input type="text" class="form-control">
+          </div>
+    </div>
+  </div>
+</nav>
+<img id="logo"src="img/1/logo.png" class="rounded mx-auto d-block" alt="logo">
 
 
-                                    <p id="li">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do iusmod tempor incididunt ut labore et dolore magna.</p>
+    <p id="li">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do iusmod tempor incididunt ut labore et dolore magna.</p>
 
-                                    <p id="li2">LOREMIPSUM DOLOR</p>
+    <p id="li2">LOREMIPSUM DOLOR</p>
 
 
   <button type="button" class="button1 mt-0">READ MORE</button>
 
-                                  </header>
-                                <main>
+</header>
+<main>
 
-                  <!-- 1 : Menu-->
+  <!-- 1 : Menu-->
 
     <section id="menu" class="container-fluid">
 <div class="d-flex justify-content-center align-items-center" id="menu">
@@ -104,45 +104,45 @@
         </div>
       </div>
     </div>
-    </section>
+  </section>
 
 <!-- 2 : Our story-->
 
 
-        <div id="ourstory" class="vh-100 d-flex align-items-center">
+      <div id="ourstory" class="vh-100 d-flex align-items-center">
 
 
-        <div class="container">
-          <div class="row">
+      <div class="container">
+        <div class="row">
 
-            <div class="col-sm-12">
-              <h2>Lorem Ipsum Dolor</h2>
-            </div>
+          <div class="col-sm-12">
+            <h2>Lorem Ipsum Dolor</h2>
+          </div>
+
+          <div class="col-sm-5 mx-auto my-3">
+          <br>
+          <p class="p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+          do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex quia voluptas sit aspernatur</p>
+          </div>
 
             <div class="col-sm-5 mx-auto my-3">
-<br>
-            <p class="p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris nisi ut aliquip ex quia voluptas sit aspernatur</p>
-            </div>
+            <br>
+            <p class="p2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip.</p>
 
-            <div class="col-sm-5 mx-auto my-3">
-<br>
-                      <p class="p2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                      do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip.
-</p>
             </div>
 
             <div class="col-lg-5 col-sm-5">
 
-                        <button type="button" class="btn">MORE INFO</button>
+            <button type="button" class="btn">MORE INFO</button>
 
-              </div>
-          </div>
         </div>
+      </div>
+    </div>
   </div>
 
 </section>
@@ -152,70 +152,69 @@
 <!-- PAGE 3: SECTION 3 -->
 
 <div id="section3" class="container-fluid">
-  <div class="container-fluid d-flex align-items-center" id="pasta6">
-  <div class="container">
-    <div class="row p-5 d-flex justify-content-center align-items-center">
+<div class="container-fluid d-flex align-items-center" id="pasta6">
+<div class="container">
+  <div class="row p-5 d-flex justify-content-center align-items-center">
 
-                 <div class="col-lg-6 col-sm-12 mt-4">
-                   <img id="pasta1"src="img/4/Img-6.png" class="img-fluid" alt="Pasta">
-                  </div>
+        <div class="col-lg-6 col-sm-12 mt-4">
+         <img id="pasta1"src="img/4/Img-6.png" class="img-fluid" alt="Pasta">
+        </div>
 
-                  <div class="col-lg-6 col-sm-12">
-                                    <h3 id="h27">Discount up to 50% All Excursions</h3>
-                                    <p id="p7"class="my-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-                                     </p>
-                                     <div class="mt-5">
-                                    <button id="bt7" type="button" class="btn mt-0">READ MORE</button>
-                                    </div>
-                                  </div>
-                                 </div>
-                               </div>
-                             </div>
+        <div class="col-lg-6 col-sm-12">
+                <h3 id="h27">Discount up to 50% All Excursions</h3>
+                <p id="p7"class="my-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                 </p>
+                 <div class="mt-5">
+                <button id="bt7" type="button" class="btn mt-0">READ MORE</button>
+                </div>
+              </div>
+             </div>
+           </div>
+         </div>
 
 
-                             <div class="container-fluid d-flex align-items-center" id="pasta7">
-                                     <div class="container">
-                                       <div class="row p-5">
+         <div class="container-fluid d-flex align-items-center" id="pasta7">
+           <div class="container">
+             <div class="row p-5">
 
-                                         <div class="col-lg-6 col-sm-12 d-flex justify-content-center align-items-start flex-column">
-                                           <h3 id="h28">January’s Promo: Buy 1 Get 1 Free !</h3>
-                                           <p id="p8"class="my-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-                                           <div class="mt-3">
-                                             <button id="bt8"type="button" class="btn mt-0">READ MORE</button>
-                                           </div>
-                                         </div>
+               <div class="col-lg-6 col-sm-12 d-flex justify-content-center align-items-start flex-column">
+                  <h3 id="h28">January’s Promo: Buy 1 Get 1 Free !</h3>
+                    <p id="p8"class="my-3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <div class="mt-3">
+                    <button id="bt8"type="button" class="btn mt-0">READ MORE</button>
+                 </div>
+               </div>
 
-                                         <div class="col-lg-6 col-sm-12 d-flex justify-content-center align-items-center mt-5">
-                                           <img id="pasta2" src="img/5/Img-7.png" class="img-fluid" alt="Pasta">
-                                         </div>
-
-                                       </div>
-                                     </div>
-                                   </div>
-                                 </div>
-                                 </section>
+            <div class="col-lg-6 col-sm-12 d-flex justify-content-center align-items-center mt-5">
+              <img id="pasta2" src="img/5/Img-7.png" class="img-fluid" alt="Pasta">
+               </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </section>
 
 <!-- 4 : PIZZA -->
 
 <section id="pizza">
+
 <div class="text-center d-flex justify-content-center" id="pizza">
 
-            <div class="container">
-            <div class="row">
+      <div class="container">
+        <div class="row">
 
-                <h2 class="mx-auto titlepizza">Lorem Ipsum Dolor Sit Amer</h2>
+          <h2 class="mx-auto titlepizza">Lorem Ipsum Dolor Sit Amer</h2>
 
-                <div class="col-lg-8 mx-auto my-4">
+          <div class="col-lg-8 mx-auto my-4">
 
-                  <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+          <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
 
-                  <img src="img/6/Rectangle.png" class="img-fluid">
+           <img src="img/6/Rectangle.png" class="img-fluid">
 
-                </div>
-
-              </div>
-            </div>
-                </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
 </section>
@@ -226,84 +225,78 @@
 
 <div id="section6" class="container-fluid">
 <div id="section6" class="d-flex justify-content-center align-items-center">
-                <div class="container p-5">
-                     <div class="row">
-                       <div class="col-lg-10 mx-auto">
+  <div class="container p-5">
+       <div class="row">
+         <div class="col-lg-10 mx-auto">
 
 
-                         <div class="row align-items-center">
-                           <div class="col-4 mx-auto">
-                             <h2 id="h22">Lorem Ipsum</h2>
-                             <br>
-                             <p id="p22">Lorem ipsum dolor sit amet, consecte sectetur adipisicing elit, tation omne ullamco laboris nisi ut aliqolore.</p>
-                           </br>
-                           </div>
+           <div class="row align-items-center">
+             <div class="col-4 mx-auto">
+               <h2 id="h22">Lorem Ipsum</h2>
+               <br>
+               <p id="p22">Lorem ipsum dolor sit amet, consecte sectetur adipisicing elit, tation omne ullamco laboris nisi ut aliqolore.</p>
+             </br>
+             </div>
 
-                           <div class="col-4 mx-auto">
-                             <h2 id="h23">Lorem Ipsum</h2>
-                             <br>
-                             <p id="p23">Lorem ipsum dolor sit amet, consecte sectetur adipisicing elit, tation omne ullamco laboris nisi ut aliqolore.</p>
-                           </br>
-                           </div>
+             <div class="col-4 mx-auto">
+               <h2 id="h23">Lorem Ipsum</h2>
+               <br>
+               <p id="p23">Lorem ipsum dolor sit amet, consecte sectetur adipisicing elit, tation omne ullamco laboris nisi ut aliqolore.</p>
+             </br>
+             </div>
 
-                           <div class="col-4 mx-auto">
-                             <h2 id="h24">Lorem Ipsum</h2>
-                             <br>
-                             <p id="p24">Lorem ipsum dolor sit amet, consecte sectetur adipisicing elit, tation omne ullamco laboris nisi ut aliqolore.</p>
-                           </br>
-                           </div>
-                         </div>
-
-
-                           <div class=" col-sm-12 text-center">
-
-                           <p id="h26">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
-<h4>Lorem Ipsum</h4>
-                           </div>
+             <div class="col-4 mx-auto">
+               <h2 id="h24">Lorem Ipsum</h2>
+               <br>
+               <p id="p24">Lorem ipsum dolor sit amet, consecte sectetur adipisicing elit, tation omne ullamco laboris nisi ut aliqolore.</p>
+             </br>
+             </div>
+           </div>
 
 
+             <div class=" col-sm-12 text-center">
+             <p id="h26">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+              <h4>Lorem Ipsum</h4>
+             </div>
 
-
-                           </div>
-                         </div>
-                       </div>
-                       </div>
-                     </div>
-                    </div>
-
- </section>
+             </div>
+           </div>
+         </div>
+         </div>
+       </div>
+     </section>
 
 
 <!-- PAGE 7 -->
 
 
-                       <div id="newsletter" class="d-flex justify-content-center align-items-center">
+     <div id="newsletter" class="d-flex justify-content-center align-items-center">
 
-                         <div class="row">
-                           <div class="col-lg-10 mx-auto text-center">
-                             <h2>Title Here</h2>
-                             <div class="row mx-auto">
-                               <div class="col-lg-10 mx-auto mx-auto mt-1 mb-4">
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p>
-                               </div>
+       <div class="row">
+         <div class="col-lg-10 mx-auto text-center">
+           <h2>Title Here</h2>
+           <div class="row mx-auto">
+             <div class="col-lg-10 mx-auto mx-auto mt-1 mb-4">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p>
+             </div>
 
-                             </div>
+           </div>
 
 
-                             <div class="row mx-auto">
-                               <div class="col-lg-10 text-align center col-sm-5">
-                                 <div class="input-group">
-                                   <input type="text" class="form-control my-0 py-1 lime-border" placeholder="Your email">
-                                    <span class="input-group-btn">
-                                      <button class="button px-4 btn-sm" type="button">SUBSCRIBE</button>
-                                    </span>
-                                 </div>
-                               </div>
-                             </div>
-                            </div>
-                         </div>
-                        </div>
-                     </section>
+           <div class="row mx-auto">
+             <div class="col-lg-10 text-align center col-sm-5">
+               <div class="input-group">
+                 <input type="text" class="form-control my-0 py-1 lime-border" placeholder="Your email">
+                  <span class="input-group-btn">
+                    <button class="button px-4 btn-sm" type="button">SUBSCRIBE</button>
+                  </span>
+               </div>
+             </div>
+           </div>
+          </div>
+       </div>
+      </div>
+   </section>
 
 
 
